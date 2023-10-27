abd3ec77833467ffe43cd683c88b281ac7dc17a2
CesareScri <117658838+CesareScri@users.noreply.github.com>
2023-10-27 19:04:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import path from "path";
 import { get } from "https";
 
 const app = express();
-const PORT = 3000;
+const PORT =  process.env.PORT || 9001;
 
 app.use(cors());
 app.use(express.static("public"));
